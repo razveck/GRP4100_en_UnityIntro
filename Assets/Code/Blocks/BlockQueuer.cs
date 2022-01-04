b569b953f7fa63f6043cfc0c1e6b8cb164bdc096
razveck <razveck@gmail.com>
2022-01-04 13:17:31 +0100

    Added queuer

--- Assets/Code/Blocks/BlockQueuer.cs
+++ Assets/Code/Blocks/BlockQueuer.cs
@@ -4,19 +4,19 @@ using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
 namespace UnityIntro.Blocks {
-	public class BlockStacker : MonoBehaviour, IPointerClickHandler {
+	public class BlockQueuer : MonoBehaviour, IPointerClickHandler {
 
 		[SerializeField]
-		private Stack<GameObject> _stack = new Stack<GameObject>();
+		private Queue<GameObject> _queue = new Queue<GameObject>();
 
 		[SerializeField]
 		private GameObject _blockPrefab;
 
 		public void OnPointerClick(PointerEventData eventData) {
-			//Push to add
-			//Pop to remove
+			//Enqueue to add
+			//Dequeue to remove
 			//Peek to read without removing
-
+			
 			//if I left click, add a block
 			//1. detect a left click
 
@@ -27,10 +27,10 @@ namespace UnityIntro.Blocks {
 			//prefered method: each Handler has its own eventData
 			if(eventData.button == PointerEventData.InputButton.Left){
 				//2. instantiate a block (ideally place it on top of the previous, or in a different place)
-				GameObject obj = Instantiate(_blockPrefab, transform.position + new Vector3(0, 0, -_stack.Count), Quaternion.identity);
+				GameObject obj = Instantiate(_blockPrefab, transform.position + new Vector3(0, 0, -_queue.Count), Quaternion.identity);
 
 				//3. add that new block to the stack
-				_stack.Push(obj);
+				_queue.Enqueue(obj);
 			}
 			
 
@@ -39,11 +39,11 @@ namespace UnityIntro.Blocks {
 			//if(Input.GetMouseButtonUp(1))
 			//	Debug.Log("Right");
 			if(eventData.button == PointerEventData.InputButton.Right){
-				if(_stack.Count == 0)
+				if(_queue.Count == 0)
 					return;
 
 				//1. pop (remove) a block from the stack
-				GameObject objectToDelete = _stack.Pop();
+				GameObject objectToDelete = _queue.Dequeue();
 				//2. delete it (the gameObject)
 				Destroy(objectToDelete);
 			}
